e5cae8f96c0d49333b277b3fc1fe6fd1b0fcbd51
Adam Shilling <admshng@gmail.com>
2020-08-07 17:53:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == false
   return true
-  if !index.between?(0, 8) == false
+  elsif !index.between?(0, 8) == false
     return true 
 else return false
     end
